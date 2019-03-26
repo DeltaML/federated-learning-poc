695622669994793466e2d88a9871c1808bed9292
agrojas <rojasagustin90@gmail.com>
2019-03-25 21:26:44 -0300

    Add server connection

--- client/data/data_loader.py
+++ client/data/data_loader.py
@@ -3,42 +3,48 @@ import logging
 import numpy as np
 from sklearn.datasets import load_diabetes
 
-seed = 43
-np.random.seed(seed)
 
+class DataLoader:
 
-def get_data(n_clients):
-    """
-    Import the dataset via sklearn, shuffle and split train/test.
-    Return training, target lists for `n_clients` and a holdout test set
-    """
-    logging.info("Loading data")
-    diabetes = load_diabetes()
-    y = diabetes.target
-    X = diabetes.data
-    # Add constant to emulate intercept
-    X = np.c_[X, np.ones(X.shape[0])]
+    def __init__(self):
+        self.X, self.y, self.X_test, self.y_test = None, None, None, None
+        self.seed = 43
+        np.random.seed(self.seed)
 
-    # The features are already preprocessed
-    # Shuffle
-    perm = np.random.permutation(X.shape[0])
-    X, y = X[perm, :], y[perm]
+    def load_data(self, n_subsets):
+        """
+        Import the dataset via sklearn, shuffle and split train/test.
+        Return training, target lists for `n_clients` and a holdout test set
+        """
+        logging.info("Loading data")
+        diabetes = load_diabetes()
+        y = diabetes.target
+        X = diabetes.data
+        # Add constant to emulate intercept
+        X = np.c_[X, np.ones(X.shape[0])]
 
-    # Select test at random
-    test_size = 50
-    test_idx = np.random.choice(X.shape[0], size=test_size, replace=False)
-    train_idx = np.ones(X.shape[0], dtype=bool)
-    train_idx[test_idx] = False
-    X_test, y_test = X[test_idx, :], y[test_idx]
-    X_train, y_train = X[train_idx, :], y[train_idx]
+        # The features are already preprocessed
+        # Shuffle
+        perm = np.random.permutation(X.shape[0])
+        X, y = X[perm, :], y[perm]
 
-    # Split train among multiple clients.
-    # The selection is not at random. We simulate the fact that each client
-    # sees a potentially very different sample of patients.
-    X, y = [], []
-    step = int(X_train.shape[0] / n_clients)
-    for c in range(n_clients):
-        X.append(X_train[step * c: step * (c + 1), :])
-        y.append(y_train[step * c: step * (c + 1)])
+        # Select test at random
+        test_size = 50
+        test_idx = np.random.choice(X.shape[0], size=test_size, replace=False)
+        train_idx = np.ones(X.shape[0], dtype=bool)
+        train_idx[test_idx] = False
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        X_train, y_train = X[train_idx, :], y[train_idx]
 
-    return X, y, X_test, y_test
+        # Split train among multiple clients.
+        # The selection is not at random. We simulate the fact that each client
+        # sees a potentially very different sample of patients.
+        X, y = [], []
+        step = int(X_train.shape[0] / n_subsets)
+        for c in range(n_subsets):
+            X.append(X_train[step * c: step * (c + 1), :])
+            y.append(y_train[step * c: step * (c + 1)])
+        self.X, self.y, self.X_test, self.y_test = X, y, X_test, y_test
+
+    def get_sub_set(self, sub_set_id):
+        return self.X[sub_set_id], self.y[sub_set_id]
--- client/service/client_service.py
+++ client/service/client_service.py
@@ -1,24 +1,35 @@
 from client.operations_utils.functions import get_deserialized_public_key
 from client.service.model_service import ModelFactory
+from client.service.server_service import ServerService
 
 
 class Client:
-    def __init__(self, name, X, y, pubkey=None):
-        self.name = name
-        self.pubkey = pubkey
+    def __init__(self, config, X, y):
+
+        self.client_id, self.client_name = config['CLIENT_ID'], config['NAME']
+        self.client_ip = config['IP']
+        self.client_port = config['PORT']
+        self.config = config
         self.X, self.y = X, y
         self.model = None
 
-    def set_public_key(self, pubkey):
-        self.pubkey = get_deserialized_public_key(pubkey)
-
     def process(self, model_type, encrypted_model):
         self.model = self.model if self.model else ModelFactory.get_model(model_type)
-        return self.model(self.name, self.X, self.y, self.pubkey).process(encrypted_model)
+        return self.model(self.client_name, self.X, self.y, self.pubkey).process(encrypted_model)
+
+    def register(self):
+        response = ServerService(self.config).register(self.get_register_data())
+        self.pubkey = get_deserialized_public_key(response['pub_key'])
+
+    def get_register_data(self):
+        return {'id': self.client_id,
+                'ip': self.client_ip,
+                'port': self.client_port
+                }
 
 
 class ClientFactory:
 
     @classmethod
-    def create_client(cls, name, X, y, pub_key=None):
-        return Client(name, X, y, pub_key)
+    def create_client(cls, name, X, y):
+        return Client(name, X, y)
