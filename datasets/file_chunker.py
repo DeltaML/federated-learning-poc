ec43216db8facbd6c140ce2a416ff01677d8fc50
Agustin Rojas <agustin.rojas@despegar.com>
2019-05-06 22:59:21 -0300

    Chunk diabetes file into files with 50 rows

---
+++ datasets/file_chunker.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    df = pd.read_csv("diabetes.csv")
+    n = 50
+    list_df = [df[i:i + n] for i in range(0, df.shape[0], n)]
+    data_frame = df.drop(index=0)
+    for idx, val in enumerate(list_df):
+        val.to_csv(path_or_buf="file_{}.csv".format(idx), index=False)
