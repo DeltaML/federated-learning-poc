abd5d85dcdfaedafc40e7f5d02dffdaa93749a7a
Agustin Rojas <agustin.rojas@despegar.com>
2019-05-23 00:35:08 -0300

    Test configuration

---
+++ federated_trainer/tests/test_data_owner_connector.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class DataOwnerConnectorTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_true(self):
+        self.assertEqual(True, True)
