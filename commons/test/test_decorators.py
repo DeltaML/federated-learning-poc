c636f8d568970c6f387ffceaae91db3e19b7780c
Agustin Rojas <agustin.rojas@despegar.com>
2019-05-25 18:44:51 -0300

    #18 | Test commons decorators ok

--- commons/test/test_decorators.py
+++ commons/test/test_decorators.py
@@ -3,7 +3,8 @@ import numpy as np
 from numpy import testing
 from unittest.mock import MagicMock
 
-from commons.decorators.decorators import optimized_collection_parameter
+from commons.decorators.decorators import optimized_collection_parameter, optimized_collection_response
+from commons.decorators.decorators import normalize_optimized_collection_argument, normalize_optimized_response
 
 
 class TestCashDecorator(unittest.TestCase):
@@ -17,7 +18,6 @@ class TestCashDecorator(unittest.TestCase):
         self.assertEqual(a(ref, data), data)
         self.assertFalse(testing.assert_array_equal(a(ref, data), np.asarray(data)))
 
-
     def test_optimized_collection_parameter_active(self):
         data = [1, 2, 3]
         ref = MagicMock()
@@ -26,5 +26,65 @@ class TestCashDecorator(unittest.TestCase):
         def a(p1, p2):
             return p2
         testing.assert_array_equal(a(ref, data), np.asarray(data))
+
+    def test_optimized_collection_response_inactive(self):
+        data = [1, 2, 3]
+        ref = MagicMock()
+
+        @optimized_collection_response(optimization=np.asarray, active=False)
+        def a(p1, p2):
+            return p2
+        testing.assert_array_equal(a(ref, data), data)
+
+    def test_optimized_collection_response_active(self):
+        data = [1, 2, 3]
+        ref = MagicMock()
+
+        @optimized_collection_response(optimization=np.asarray, active=True)
+        def a(p1, p2):
+            return p2
+        testing.assert_array_equal(a(ref, data), np.asarray(data))
+
+    def test_normalize_optimized_collection_inactive(self):
+        data = [1, 2, 3]
+        np_data = np.asarray(data)
+        ref = MagicMock()
+
+        @normalize_optimized_collection_argument(active=False)
+        def a(p1, p2, p3):
+            return p3
+        testing.assert_array_equal(a(ref, data, np_data), np_data)
+
+    def test_normalize_optimized_collection_active(self):
+        data = [1, 2, 3]
+        np_data = np.asarray(data)
+        ref = MagicMock()
+
+        @normalize_optimized_collection_argument(active=True)
+        def a(p1, p2, p3):
+            return p3
+        testing.assert_array_equal(a(ref, data, np_data), data)
+
+
+
+    def test_normalize_optimized_response_inactive(self):
+        data = [1, 2, 3]
+        np_data = np.asarray(data)
+        ref = MagicMock()
+
+        @normalize_optimized_response(active=False)
+        def a(p1, p2, p3):
+            return p3
+        testing.assert_array_equal(a(ref, data, np_data), np_data)
+
+    def test_normalize_optimized_response_active(self):
+        data = [1, 2, 3]
+        np_data = np.asarray(data)
+        ref = MagicMock()
+
+        @normalize_optimized_response(active=True)
+        def a(p1, p2, p3):
+            return p3
+        testing.assert_array_equal(a(ref, data, np_data), data)
 
 
